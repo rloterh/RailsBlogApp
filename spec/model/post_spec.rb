e92a533c4033f5370c92091c9fa82c37ff36e245
Robert Loterh <rloterh@gmail.com>
2023-05-21 14:25:20 +0000

    Add specs for post model

---
+++ spec/model/post_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  subject { Post.new(title: 'AI Generation', text: 'Lorem ipsum dolor sit amet', comments_counter:'5', likes_counter: -4) }
+
+  before { subject.save }
+  
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it 'text should be present' do
+    subject.text = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it 'comments_counter to be present' do
+    subject.comments_counter = '5'
+    expect(subject).to_not be_valid
+  end
+  
+  it 'likes_counter to be an integer' do
+    subject.likes_counter = -4
+    expect(subject).to_not be_valid
+  end
+
+end
