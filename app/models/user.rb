cb1e1bc9ac94045356d9b705b630c63400349ee7
Robert Loterh <rloterh@gmail.com>
2023-05-21 04:31:35 +0000

    Add validattions to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
-  has_many: likes, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+
+  validates :name, presence: true
+  validates :name, length: { minimum: 250 }
+  validates :posts_counter, inumericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order('created_at Desc').limit(3)
