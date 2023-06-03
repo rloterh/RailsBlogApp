2d7a9955fddea265a82758a3015c6fdcad81200d
Robert Loterh <rloterh@gmail.com>
2023-06-03 20:00:25 +0000

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :name, length: { minimum: 250 }
+  validates :name, length: { maximum: 250 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
